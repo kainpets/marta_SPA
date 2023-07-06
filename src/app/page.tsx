cbccba3bad78cf0ae4e3a5b10ed414b73bc6b60a
pawel_stepniak <pawelstepniak22@gmail.com>
2023-07-06 23:52:08 +0200

    uslugi section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import juliaMilosz from "/public/Julia&Miłosz_672.jpg"
 import martaKciuki from "/public/martazkciukami.jpg"
+import martalabedz from "/public/martalabedz.jpg"
+import martaradzi from "/public/martaradzi.jpg"
+import martanogadogory from "/public/martanogadogory.jpg"
 
 export default function Home() {
   return (
@@ -20,6 +23,7 @@ export default function Home() {
           <h5 className="text-3xl leading-snug">ABY PIERWSZY TANIEC <br></br> BYŁ DLA WAS PRZYGODĄ</h5>
         </div>
       </div>
+
       {/* o mnie */}
       <div className='sm:flex pt-8 px-4'>
         <div className="flex flex-col gap-1">
@@ -43,7 +47,55 @@ export default function Home() {
           <p>
             Przygotowuję około 25 par rocznie do pierwszego tańca. Mam ogromne szczęście trafiać na wspaniałych ludzi! </p>
         </div>
-        <Image src={martaKciuki} sizes='100vw' placeholder="blur" alt="Uśmiechnięta Marta z dwoma kciukami w górze" className='object-scale-down' />
+        <Image src={martaKciuki} sizes='100vw' placeholder="blur" alt="Uśmiechnięta Marta z dwoma kciukami w górze" className='object-scale-down sm:max-w-fit' />
+      </div>
+
+      {/* usługi */}
+      <div style={{
+        display: "grid",
+        gridGap: "16px",
+        gridTemplateColumns: "repeat(auto-fit, minmax(300px, auto))",
+        paddingTop: "4rem",
+        alignItems: "baseline",
+        paddingRight: "1rem",
+        paddingLeft: "1rem",
+      }}>
+        <div style={{ position: "relative", height: "625px" }}>
+          <Image src={martanogadogory} alt="Marta tańczy z nogą uniesioną do góry" className="shadow" placeholder="blur" sizes="100vw" style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "70%"
+          }} />
+          <div className="pt-4">
+            <h2 className="text-center text-xl pb-1">WARSZTATY TANECZNE</h2>
+            <p className="text-justify">Prowadzę zajęcia taneczne na eventach, warsztaty dla osób początkujących oraz tancerzy. Zapraszam na grupowe zajęcia taneczne dla wszystkich grup zaawansowania. </p>
+          </div>
+        </div>
+        <div style={{ position: "relative", height: "625px" }}>
+          <Image src={martaradzi} alt="Para tańczy na pierwszym planie, Marta doradza w tle" className="shadow" placeholder="blur" sizes="100vw" style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "70%"
+          }} />
+          <div className="pt-4">
+            <h2 className="text-center text-xl pb-1">PIERWSZY TANIEC</h2>
+            <p className="text-justify">Pierwszy taniec to głównie lekcje indywidualne dla par młodych. Zaczynamy od podstawowych kroków tańca użytkowego, potem przechodzimy do przygotowanej dla Was choreografii. </p>
+          </div>
+        </div>
+        <div style={{ position: "relative", height: "625px" }}>
+          <Image src={martalabedz} alt="Marta tańczy we wdzięcznej pozie" className="shadow" placeholder="blur" sizes="100vw" style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "70%"
+          }} />
+          <div className="pt-4">
+            <h2 className="text-center text-xl pb-1">TANIEC DLA KOBIET</h2>
+            <p className="text-justify">Zajęcia pełne kobiecości, na których każda z Was może poczuć się jak gwiazda Broadway&apos;u. Prowadzę zajęcia grupowe i indywidualne. Realizuję też tańce na wieczorach panieńskich.</p>
+          </div>
+        </div>
+      </div>
+      <div className="marginx-auto w-full text-center pb-2">
+        <h3 className="text-xl">Chcesz dowiedzieć się więcej? <a href='tel:793 792 883' className="font-bold">Zadzwoń!</a></h3>
       </div>
 
     </main>
